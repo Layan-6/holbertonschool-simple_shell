7022019dd3a49beb1a5882850dfcadf29e8baac9
Layan-6 <11956@holbertonstudents.com>
2025-11-09 20:00:09 +0300

    Expand shell.h with additional function prototypes
    Added function prototypes for shell loop and command execution.

--- shell.h
+++ shell.h
@@ -1,8 +1,23 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-void print_env(void);
-char *find_command_path(char *command);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* Function prototypes */
+void shell_loop(char **env);
+char *read_line(void);
+char **parse_line(char *line);
+int execute_args(char **args, char **env);
+int check_builtins(char **args, char **env);
+
+/* Built-in functions */
+void exit_shell(char **args);
+void print_env(char **env);
 
 #endif /* SHELL_H */
-EOF
