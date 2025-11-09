e3209797e11d5b9a8246d72e7c9298898096bccf
Afnan9999 <afnan-911@outlook.sa>
2025-11-09 23:42:20 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,9 +1,9 @@
-#ifndef SHELL_H
-#define SHELL_H
+  #ifndef SHELL_H
+  #define SHELL_H
 
-extern char **environ;
+  extern char **environ;
 
-void print_env(void);
-char *find_command_path(char *command);
+  void print_env(void);
+  char *find_command_path(char *command);
 
-#endif /* SHELL_H */
+  #endif /* SHELL_H */
