5655e0d26791fa41c081a86385cbcbc1e1082e6f
Afnan9999 <afnan-911@outlook.sa>
2025-11-10 16:36:52 +0300

    Update env.c

--- env.c
+++ env.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-#include "shell.h"
+
+extern char **environ;
 
 /**
- * print_env - Prints the current environment variables
+ * print_env - تطبع جميع متغيرات البيئة الحالية
  */
 void print_env(void)
 {
     int i = 0;
+
     while (environ[i])
     {
         printf("%s\n", environ[i]);
