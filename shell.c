a27048f7fb3d842f4755c4258756ea0af1dc2d0c
Afnan9999 <afnan-911@outlook.sa>
2025-11-11 18:15:21 +0300

    shell.c

--- shell.c
+++ shell.c
@@ -1,10 +1,13 @@
 #include "shell.h"
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * shell_loop - Main loop of the shell
+ * @av: Argument vector (used for shell name)
  * @env: Environment variables
  */
-void shell_loop(char **env)
+void shell_loop(char **av, char **env)
 {
     char *line = NULL;
     size_t len = 0;
@@ -22,8 +25,13 @@ void shell_loop(char **env)
         }
 
         args = tokenize(line);
-        if (args[0] != NULL)
-            execute(args, env);
+        if (args && args[0])
+        {
+            if (_strcmp(args[0], "env") == 0)
+                print_env(env);
+            else
+                execute(args, av, env);
+        }
 
         free(args);
     }
