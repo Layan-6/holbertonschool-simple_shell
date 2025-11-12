a506ae3e88b58e0fff1cb8f32fd09ccaabf6b985
layan <layansaleem@gmail.com>
2025-11-12 15:21:39 +0000

    2

--- shell.c
+++ shell.c
@@ -1,21 +1,104 @@
-cat > shell.c << 'EOF'
 #include "shell.h"
 
 /**
  * main - Entry point for simple shell
- * @argc: Argument count
- * @argv: Argument vector
- * @env: Environment variables
  *
- * Return: 0 on success
+ * Return: Always 0
  */
-int main(int argc, char **argv, char **env)
+int main(void)
 {
-(void)argc;
-(void)argv;
-(void)env;
+    char *input;
 
-printf("Simple Shell Starting...\n");
+    while (1)
+    {
+        display_prompt();
+        input = read_input();
+        
+        if (input == NULL)
+        {
+            printf("\n");
+            break;
+        }
+        
+        if (strlen(input) > 0)
+        {
+            execute_command(input);
+        }
+        
+        free(input);
+    }
+    
+    return (0);
+}
 
-return (0);
+/**
+ * display_prompt - Displays the shell prompt
+ */
+void display_prompt(void)
+{
+    printf("#cisfun$ ");
+    fflush(stdout);
+}
+
+/**
+ * read_input - Reads a line of input from user
+ *
+ * Return: Pointer to the input string
+ */
+char *read_input(void)
+{
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread;
+
+    nread = getline(&line, &len, stdin);
+    
+    if (nread == -1)
+    {
+        free(line);
+        return (NULL);
+    }
+    
+    /* Remove newline character */
+    if (line[nread - 1] == '\n')
+        line[nread - 1] = '\0';
+    
+    return (line);
+}
+
+/**
+ * execute_command - Executes a command using execve
+ * @command: The command to execute
+ *
+ * Return: 1 on success, 0 on failure
+ */
+int execute_command(char *command)
+{
+    pid_t pid;
+    int status;
+    char *args[] = {command, NULL};
+
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("Error");
+        return (0);
+    }
+    
+    if (pid == 0)
+    {
+        /* Child process */
+        if (execve(command, args, environ) == -1)
+        {
+            printf("./shell: No such file or directory\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        /* Parent process */
+        wait(&status);
+    }
+    
+    return (1);
 }
--- shell.h
+++ shell.h
@@ -9,16 +9,11 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
+extern char **environ;
+
 /* Function prototypes */
 void display_prompt(void);
 char *read_input(void);
-char **parse_input(char *input);
-int execute_command(char **args);
-int check_builtins(char **args);
-
-/* Built-in commands */
-int shell_exit(char **args);
-int shell_env(char **args);
-int shell_cd(char **args);
+int execute_command(char *command);
 
 #endif
